0a227cd7a78680460d60fe1d95fa53939ea37543
Tim Bernhard <tim@bernhard-webstudio.ch>
2019-06-10 14:46:00 +0200

    fix last issues & get the script working

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 const settings = JSON.parse(fs.readFileSync(__dirname + '/../settings.json'));
+var results = {};
+var debug = true;
 
 // start fetching folders
 (async () => {
-    var results = {};
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: !debug,
         // slowMo: 250 // slow down by 250ms 
     });
     const page = await browser.newPage();
+    if (debug) {
+        page.setViewport({ width: 0, height: 0 });
+    }
     await page.goto('https://quizlet.com/latest');
     // login if necessary
     if (page.url() !== 'https://quizlet.com/latest') {
@@ -30,53 +34,79 @@ const settings = JSON.parse(fs.readFileSync(__dirname + '/../settings.json'));
             page.click(".LoginPromptModal-form button[type='submit']")
         ]);
     }
+    const folderSelector = ".ProfileFoldersPage .DashboardListItem";
     // go to folder page
-    await page.goto('https://quizlet.com/' + settings.username + '/folders');
+    await Promise.all([
+        page.waitForNavigation(),
+        page.waitForSelector(folderSelector),
+        page.goto('https://quizlet.com/' + settings.username + '/folders')
+    ]);
+
     // find all folders
-    const folders = await page.$$(".ProfileFoldersPage .DashboardListItem");
-    var promisses = [];
-    folders.forEach(folderLink => {
-        promises.push(handleFolder(page, folderLink));
+    // resp. their properties
+    const folderNames = await page.$$eval(folderSelector + " header.FolderPreview-cardHeader", (headers) => headers.map(header => header.innerText));
+    const folderLinks = await page.$$eval(folderSelector + " .UILinkBox-link a", (links) => links.map(link => link.href));
+
+    console.log("Found " + folderLinks.length + " folders...");
+
+    // process all folders
+    await asyncForEach(folderLinks, async (folderLink, index) => {
+        await handleFolder(page, folderLink, folderNames[index]);
     });
-    await Promise.all(promisses);
     // output results
     console.log(results);
 
     await browser.close();
 })();
 
-async function handleFolder(page, folderLink) {
+async function handleFolder(page, folderLink, folderName) {
+    const setSelector = ".FolderPageSetsView .DashboardListItem";
     // go to folder page
-    const folderLinkHeader = await folderLink.$("header.FolderPreview-cardHeader");
-    const folderName = await (await folderLinkHeader.getProperty('textContent')).jsonValue();
     const [] = await Promise.all([
         page.waitForNavigation(),
-        folderLink.click("a")
+        page.waitForSelector(setSelector),
+        page.goto(folderLink)
     ]);
     results[folderName] = {};
     // find all sets in this folder
-    const sets = await page.$$(".FolderPageSetsView .DashboardListItem");
-    var promisses = [];
-    sets.forEach(setLink => {
-        promises.push(handleSet(page, setLink));
+    // resp. their properties
+    const setNames = await page.$$eval(setSelector + " header.SetPreview-cardHeader", (headers) => headers.map(header => header.innerText));
+    const setLinks = await page.$$eval(setSelector + " .UILinkBox-link a", (links) => links.map(link => link.href));
+
+    console.log("Found " + setNames.length + " sets in Folder '" + folderName + "'...");
+    // process all sets
+    await asyncForEach(setLinks, async (setLink, index) => {
+        await handleSet(page, setLink, folderName, setNames[index]);
     });
-    await Promise.all(promisses);
 };
 
 
-async function handleSet(page, setLink) {
-    const setLinkHeader = await folderLink.$("header.SetPreview-cardHeader");
-    const setName = await (await setLinkHeader.getProperty('textContent')).jsonValue();
-
+async function handleSet(page, setLink, folderName, setName) {
     // got to set page
+    const setOptionsSelector = ".SetPage-menuOption .UIIcon--more";
+    const exportButtonIconSelector = ".UIIcon--export";
     const [] = await Promise.all([
         page.waitForNavigation(),
-        setLink.click("a")
+        page.waitForSelector(setOptionsSelector),
+        page.goto(setLink)
     ]);
-    const exportButtonIcon = await page.$("UIIcon--export");
-    await exportButtonIcon.click();
-    const exportModal = await page.waitForSelector(".SetPageExportModal-content");
-    const exportTextarea = await exportModal.$("textarea.UITextarea-textarea");
-    const text = await (await exportTextarea.getProperty('textContent')).jsonValue();
+    const [] = await Promise.all([
+        page.waitForSelector(exportButtonIconSelector),
+        page.hover(setOptionsSelector)
+    ]);
+    const exportModalSelector = ".SetPageExportModal-content";
+    const [] = await Promise.all([
+        page.waitForSelector(exportModalSelector),
+        page.click(exportButtonIconSelector)
+    ]);
+    const exportTextarea = await page.$(exportModalSelector + " textarea.UITextarea-textarea");
+    const textValue = await exportTextarea.getProperty('value');
+    const text = await textValue.jsonValue();
     results[folderName][setName] = text;
+}
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
 }
